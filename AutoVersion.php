55acd9c601fa12f7472f5f2a4c2e39fd1e08e6db
Jeff Moser <jeff@bitsalt.com>
2017-11-16 09:08:49 -0500

    Adding AutoVersion class to utilities.

---
+++ AutoVersion.php
@@ -0,0 +1,45 @@
+<?php
+
+/**********
+  Never worry about clients using cached versions of files.
+
+  This class is based on a community discussion from stackoverflow.com. View the
+  thread at:
+  https://stackoverflow.com/questions/118884/how-to-force-browser-to-reload-cached-css-js-files
+ 
+  Use of this class requires the following modification to .htaccess
+ 
+  RewriteEngine On
+  RewriteRule ^(.*)\.[\d]{10}\.(css|js)$ $1.$2 [L] 
+ 
+  Anywhere you call a .css or .js file, wrap it with a call to the static version() function.
+  This has the same effect of versioning without having to change references 
+  to the files every time a file is updated. Clients will never use cached versions
+  of files that have been changed on the server.
+  
+  Example...given this call in the layout:
+  
+  Zend example
+  $this->headScript()->prependFile($this->basePath() . AutoVersion::version('/js/application.js'))
+
+  Plain html example
+  <script type="text/javascript" src="<?php AutoVersion::version('/js/application.js') ?>"></script>
+
+  
+  The client sees this:
+
+  <script type="text/javascript" src="/js/application.1509984994.js"></script>
+
+ ***********/
+class AutoVersion {
+    
+    public static function version($filePath) {
+        if(strpos($filePath, '/') !== 0 || !file_exists($_SERVER['DOCUMENT_ROOT'] . $filePath)) {
+            return $filePath;
+        }
+        
+        $modTime = filemtime($_SERVER['DOCUMENT_ROOT'] . $filePath);
+        
+        return preg_replace('{\\.([^./]+)$}', ".$modTime.\$1", $filePath);
+    }
+}
